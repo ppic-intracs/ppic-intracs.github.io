15278f755ab44fe0c94ed3b0c63c9289a06a27ec
ppic-intracs <ppic-intracs@outlook.com>
2024-08-20 10:46:21 +0700

    Index.php
    Index.php

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Membuat design table bergaya elegan dengan CSS | MalasNgoding.com</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<center><h1>Data Stock PPIC</h1></center>
+	<table width=100% cellspacing='0'>
+		<thead>
+			<tr>
+				<th width="10%">Kode</th>
+				<th width="70%">Nama Barang</th>
+				<th width="20%">Stock</th>
+				<th width="10%">Satuan</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>B84</td>
+				<td>Jakarta Selatan</td>
+				<td>Web Designer</td>
+				<td>21</td>
+			</tr>
+			<tr>
+				<td>Budi</td>
+				<td>Bandung</td>
+				<td>Web Developer</td>
+				<td>26</td>
+			</tr>
+			<tr>
+				<td>Cahyo</td>
+				<td>Bekasi</td>
+				<td>Web Programmer</td>
+				<td>29</td>
+			</tr>
+			<tr>
+				<td>Darma</td>
+				<td>Bali</td>
+				<td>Teknisi</td>
+				<td>35</td>
+			</tr>
+		</tbody>
+	</table>
+</body>
+</html>
